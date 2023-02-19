1c41dacc754d8673ad8f58d735110d2b19c45631
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-02-19 17:29:08 +0530

    added options object with threshold

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const cards = document.querySelectorAll(".card");
 
 const entriesCallback = (entries) => {
   console.log(entries);
-  entries.forEach((entry) =>
-    entry.target.classList.toggle("show", entry.isIntersecting)
-  );
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("show", entry.isIntersecting);
+    if (entry.isIntersecting) observer.unobserve(entry.target); // If its visible on screen or intersecting => unobserve it. Used when you don't want to show animation again when scrolling up again
+  });
 };
 
-const observer = new IntersectionObserver(entriesCallback);
+const observer = new IntersectionObserver(entriesCallback, {
+  threshold: 1,
+});
 
 cards.forEach((card) => observer.observe(card));
