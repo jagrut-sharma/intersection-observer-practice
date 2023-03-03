cc91cb2688f8b23cd1dccd2a3024b7fca2cf5bc6
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-03-03 11:05:47 +0530

    changed port

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const entriesCallback = (entries) => {
 };
 
 const optionsObj = {
-  //   root: null, // element that target element intersects => null = viewport => If set to parent element, it tracks all children element inside of it --> Not used much
+  //   root: null, // element that target element intersects => null(default) = viewport => If set to parent element, it tracks all children element inside of it --> Not used much
   threshold: 1,
   //   rootMargin: "-150px", // will reduce the container size by 150px and cards will be visible in less area.
   //   rootMargin: "100px", // will increase the container size by 100px and cards will be loaded as long as its near 100px to visible/intersection area.
